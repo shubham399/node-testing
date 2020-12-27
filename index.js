59b1ba3f014549e10bf094be3b92c296b8a4cb7b
Shubham Kumar <hello@shubhkumar.in>
2020-12-27 21:49:52 +0530

    Fix Test In a way.

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
   res.send('URL Shortner UP')
 })
 
+app.post('/',(req,res) =>{
+  res.json({"url":"hello","shortURL":"hello"})
+})
+
+app.get('/:id',(req,res) =>{
+  res.redirect("http://google.com")
+})
+
 const server = app.listen(port)
 
 module.exports = { app, server };
