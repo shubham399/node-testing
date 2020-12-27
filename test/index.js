31e14dedf8f7057859a5943ef2414d756471c6bf
Shubham Kumar <hello@shubhkumar.in>
2020-12-27 21:59:24 +0530

    Test To Consider res location for Redirection.

--- test/index.js
+++ test/index.js
@@ -11,14 +11,16 @@ describe("Index Test", function(){
         assert.strictEqual(res.text, "URL Shortner UP", 'Body Should be `"URL Shortner UP"`');
     })    
     it("Try Adding a New URL and Test for if That short URL is Redirecting", async function(){
-        let res = await request(app).post("/").send({"url":"http://shubhkumar.in"});
+        let url = "http://shubhkumar.in"
+        let res = await request(app).post("/").send({"url":url});
         assert.strictEqual(res.status, 200, 'Status is not 200');
-        assert.hasAllKeys(res.body, ["url","shortURL"], 'Body Should have `url` and `shortURL` in Response');
+        assert.hasAllKeys(res.body, ["url","id"], 'Body Should have `url` and `shortURL` in Response');
         // Check if that Short URL is Redirecting
 
-        res = await request(app).get(`/${res.body.shortURL}`);
+        res = await request(app).get(`/${res.body.id}`);
+        console.log(res.headers.location);
         assert.strictEqual(res.status, 302, 'Status is not 302');
-        assert.equal(res.status, 302, 'Status is not 302');
+        assert.equal(res.headers.location, url, `Redirection should happen to ${url} and not ${res.headers.location}`);
     })
 })
 
